c50a4ef6a2e6491f06e8542e034e175be99d6dc1
John Hernandez <Johnhndzr@gmail.com>
2017-05-21 20:02:49 -0500

    changes in class
    add new transpose in the matrix

--- diferenciasDivididas.java
+++ diferenciasDivididas.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 public class diferenciasDivididas{
 	float xK, xZero;
-	int valuesQty;
+	int valuesQty, positionOfXZero;
 	float[] x;
 	float[] fx;
 	float[][] table;
+	float[] differences;
 
 	public size(int a){
-		//This is a constructor to set the arrays size, this one may receive valuesQty as a parameter
+		//This is a constructor to set the arrays size, this one may 
+		//receive valuesQty as a parameter
 		table = new float[a][a];
 		x = new float[a];
 		fx = new float[a];
+		differences = new float[a];
 	}
 
 	public int makeTable(float[] a, float[] b){
@@ -21,6 +24,7 @@ public class diferenciasDivididas{
 		//array a[] contains values for X & array b[] contains values for F(x)
 		int n = a.length, i, j, position;
 		float[][] table = new float[a.length][a.length];
+		float[][] tableAux = new float[a.length][a.length];
 		//The loop is going to populate the table array with the values provided for F(x)
 		for (i = 0; i < n; i++ ) {
 			table[i][0] = b[i];			
@@ -33,11 +37,23 @@ public class diferenciasDivididas{
     	  		position++;
    		   	}
       	}
+      	//Making the transpose of the matrix
+      	for (i = 0 ; i < n ;i++) {
+      		for (j = 0; j < n ;j++) {
+      			tableAux[j][i] = table[i][j];
+      		}
+      	}
+      	for (i = 0 ; i < n ;i++) {
+      		for (j = 0; j < n ;j++) {
+      			table[j][i] = tableAux[j][i];
+      		}
+      	}
       	return 0;	
 	}
 
 	public int positionToInterpolate(float a, float[] b){
-		//This metod should deliver the position where the element has to be interpolated
+		//This metod should deliver the position where the element has to be 
+		//interpolated, may receive the value to interpolate & the x[] array
 		int position = 0, i, n = b.length;
 		for (i = 0 ; i < n ; i++ ) {
 			if ((a >= b[i])) && (a >= b[i+1]) {
@@ -46,14 +62,21 @@ public class diferenciasDivididas{
 		}
 		return position;
 	}
-	//This metod should makes the interpolation of Xk trough the table
-	public int forwardInterpolation(int xZero, float[] table, float xK){
-		int i = 0, j = 0;
-		float currentValue = 0, totalValue;
+/*	public int forwardInterpolation(float xZero,  float xK,, float[] table,int positionOfXZero){
+		//This metod should makes the interpolation of Xk(value to interpolate) through the table may receive 
+		//the Xzero, Xk, the array of the differences and the position of Xzero
+		int i = (positionOfXZero+1), j = 0, n = table.length;
+		float currentValue = 0, totalValue = 0;
+      	//This part populates the differences array to use it in the interpolation
+      	for (i = 0; i < n ;i++) {
+      		
+      	}
+		currentValue = table[positionOfXZero][0];
 		for (i ; i < n ; i++) {
-			for (j=;j ;j+) {
+
+			for (j=;j ;j++) {
 				
 			}
 		}
-	}
+	}*/
 }
